70140abfb4017b34281dd045f678c7396276c98b
kimdragonhee <kim.dragon.hee@gmail.com>
2020-03-02 17:15:56 +0900

    27일 버전으로 revert

--- src/main/java/com/chuno/found/FoundApplication.java
+++ src/main/java/com/chuno/found/FoundApplication.java
@@ -2,12 +2,10 @@ package com.chuno.found;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaAuditing //createAt, updatedAt 사용시  넣어줘야함. jpa Autiting 어노테이션 활성화하는 어노테이션
-@EnableJpaRepositories //spring boot 사용시 사용하지 않아도 자동 설정됨.
 @SpringBootApplication
+@EnableJpaRepositories //spring boot 사용시 사용하지 않아도 자동 설정됨.
 public class  FoundApplication {
 
     public static void main(String[] args) {
