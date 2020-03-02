7adbdda0aa79a55fb1944fad8519bf500f8946d8
kimdragonhee <kim.dragon.hee@gmail.com>
2020-03-02 16:36:30 +0900

    test

--- src/main/java/com/chuno/found/findService/Find.java
+++ src/main/java/com/chuno/found/findService/Find.java
@@ -1,6 +1,7 @@
 package com.chuno.found.findService;
 
 
+import com.chuno.found.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Find {
+@Entity
+public class Find extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
